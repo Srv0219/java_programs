ede305c14067db79f57cac254cddeaddd16e18f6
Sourav <adak.sourav2@gmail.com>
2021-10-19 19:08:09 +0530

    450 question array

--- src/com/java/srv/fourFifty/FactorialsOfLargeNumber2.java
+++ src/com/java/srv/fourFifty/FactorialsOfLargeNumber2.java
@@ -1,8 +1,6 @@
 package com.java.srv.fourFifty;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class FactorialsOfLargeNumber2 {
     public static void main(String[] args) {
@@ -10,23 +8,23 @@ public class FactorialsOfLargeNumber2 {
         factorialLargerNumber(n);
     }
 
-    private static  ArrayList<Integer>  factorialLargerNumber(int n) {
+    private static ArrayList<Integer> factorialLargerNumber(int n) {
 
-        ArrayList<Integer> result=new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
         result.add(1);
-        int carry=0;
-        int val=2;
-        while(val<=n){
+        int carry = 0;
+        int val = 2;
+        while (val <= n) {
 
-            for(int i=result.size()-1;i>=0;i--){
+            for (int i = result.size() - 1; i >= 0; i--) {
 
-                int temp=result.get(i)*val+carry;
-                result.set(i,temp%10);
-                carry=temp/10;
+                int temp = result.get(i) * val + carry;
+                result.set(i, temp % 10);
+                carry = temp / 10;
             }
-              while(carry!=0){
-                result.add(0,carry%10);
-                carry=carry/10;
+            while (carry != 0) {
+                result.add(0, carry % 10);
+                carry = carry / 10;
             }
             val++;
 
