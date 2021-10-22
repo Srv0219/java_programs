9e013801fc1b2f51fc690361582f8a3c9b25e465
Sourav <adak.sourav2@gmail.com>
2021-10-22 13:04:09 +0530

    450 question array

--- src/com/java/srv/fourFifty/ElementsThatAppearMoreThannkTimes2.java
+++ src/com/java/srv/fourFifty/ElementsThatAppearMoreThannkTimes2.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class ElementsThatAppearMoreThannkTimes2 {
     public static void main(String[] args) {
-        int arr[] = {1, 1, 2, 2, 3, 5, 5, 5, 5, 5, 4, 2, 2, 3, 1, 1, 1};
+        int arr[] = {1, 1, 2, 2, 3, 5, 5, 5, 8, 7, 4, 2, 2, 3, 1, 1, 1};
         int n = arr.length;
         int k = 4;
         appearElements(arr, n, k);
@@ -15,19 +15,17 @@ public class ElementsThatAppearMoreThannkTimes2 {
         int str = 0;
         int count = 1;
         int div = n / k;
-        for (int i = 0; i < arr.length-1 ; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] == arr[i + 1]) {
                 count++;
             } else {
                 if (count > div) {
                     System.out.println(arr[i] + " " + count);
                     count = 1;
-
                 }
                 count = 1;
-
             }
-            str=arr[i];
+            str = arr[i];
         }
         if (count > div) {
             System.out.println(str + " -> " + count);
---
+++ src/com/java/srv/fourFifty/MaxProfitByBuyingSellingTwice.java
@@ -0,0 +1,22 @@
+package com.java.srv.fourFifty;
+
+public class MaxProfitByBuyingSellingTwice {
+    public static void main(String[] args) {
+        int arr[] = { 2, 30, 15, 10, 8, 25, 80 };
+
+        stockBuyingSellingTwice(arr);
+    }
+
+    private static void stockBuyingSellingTwice(int[] arr) {
+        int profit=0;
+        for(int i=1;i< arr.length;i++){
+            int sun=arr[i]-arr[i-1];
+            if (sun>0){
+                profit=profit+sun;
+            }
+        }
+        System.out.println(profit);
+    }
+
+
+}
